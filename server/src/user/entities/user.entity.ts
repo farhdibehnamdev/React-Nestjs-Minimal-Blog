1114aa486f9dcf24685f20f10bd727ba1c1e4e50
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-11-14 22:54:42 +0330

    🔧 chore:Added user entity.

---
+++ server/src/user/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export default class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  passwordConfirm: string;
+}
