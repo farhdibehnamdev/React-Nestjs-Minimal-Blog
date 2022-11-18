452102fe57c1bbad6980b82cfd2d9d1cb3688c5f
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-11-18 21:25:02 +0330

    👔 chore:Implemented CRUD category service.

---
+++ server/src/category/category.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/Category.entity';
+
+@Injectable()
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  async findAll() {
+    const categories = await this.categoryRepository.find();
+    if (!categories) throw new NotFoundException("There isn't any Category.");
+    return categories;
+  }
+
+  async findOne(id: number) {
+    const category = await this.categoryRepository.findOne({ where: { id } });
+    if (!category) throw new NotFoundException('Category Not Found!!!');
+
+    return category;
+  }
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    const category = this.categoryRepository.create(createCategoryDto);
+    return await this.categoryRepository.save(category);
+  }
+
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    const categoryFound = await this.categoryRepository.findOne({
+      where: { id },
+    });
+    if (!categoryFound) throw new NotFoundException('Category Not Found!!');
+    Object.assign(categoryFound, updateCategoryDto);
+    return this.categoryRepository.save(categoryFound);
+  }
+
+  async remove(id: number) {
+    const category = await this.categoryRepository.findOne({ where: { id } });
+    if (!category) throw new NotFoundException('Category Not Found!!');
+    return this.categoryRepository.remove(category);
+  }
+}
