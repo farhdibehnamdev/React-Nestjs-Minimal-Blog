cb5f3e0ca7192ac6b74551c4e744be30b0263cfa
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-11-19 07:41:45 +0330

    ♻️ refactor:Refactored environment variable.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,24 +4,27 @@ import { CategoryModule } from './category/category.module';
 import { TagController } from './tag/tag.controller';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TagService } from './tag/tag.service';
 import { TagModule } from './tag/tag.module';
-
+import { validate } from './config/env.validation';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'blogdb',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true, validate }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     ArticleModule,
     CategoryModule,
