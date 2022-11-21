4074f84e968c4a18f7d6196febf40dd216ff8f45
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-11-21 22:17:32 +0330

    🔧 chore:Added global customization style theme.

--- admin-panel/src/styles/theme.ts
+++ admin-panel/src/styles/theme.ts
@@ -2,6 +2,38 @@ import { createTheme } from "@mui/material";
 
 const theme = createTheme({
   direction: "rtl",
+  // typography: {
+  //   allVariants: {
+  //     fontFamily: "IRANYekan",
+  //     textTransform: "none",
+  //     fontSize: 13.36,
+  //   },
+  //   subtitle1: {
+  //     fontSize: 14,
+  //   },
+  //   body: {
+  //     fontFamily: "Poppins",
+  //     fontSize: 15,
+
+  //     fontWeight: "bold",
+  //   },
+  // },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        html: {
+          fontFamily: "IRANYekan",
+          textTransform: "none",
+          fontSize: 16,
+        },
+        body: {
+          fontFamily: "IRANYekan",
+          fontSize: 16,
+          fontWeight: "bold",
+        },
+      },
+    },
+  },
 });
 
 export default theme;
