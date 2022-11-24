979b13fac82e1f00fd7c6e682de7ecc9d4515941
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-11-24 20:52:03 +0330

    ♻️ refactor:Refactored styles from inline to external styels.

--- admin-panel/src/components/card/Card.style.ts
+++ admin-panel/src/components/card/Card.style.ts
@@ -1,1 +1,29 @@
 import { SxProps } from "@mui/material";
+
+const paperCardStyle: SxProps = {
+  background: "#fff",
+  borderRadius: "10px",
+  border: "none",
+  flex: "1 1 auto",
+  ".boxCardContainerStyle": {
+    alignItems: "center",
+    paddingRight: "15px",
+    paddingLeft: "15px",
+    ".linearProgressStyle": {
+      borderRadius: "20px",
+    },
+  },
+  ".stackColumnPaperStyle": {
+    gap: "30px",
+    padding: "20px",
+    ".stackColumnPaperStyle": {
+      justifyContent: "space-around",
+      alignItems: "center",
+      ".typographyContentPaperStyle": {
+        fontSize: "20px",
+      },
+    },
+  },
+};
+
+export default paperCardStyle;
--- admin-panel/src/components/card/Card.tsx
+++ admin-panel/src/components/card/Card.tsx
@@ -3,29 +3,19 @@ import { Box, Grid, Paper, Stack, Typography } from "@mui/material";
 import LinearProgress, {
   LinearProgressProps,
 } from "@mui/material/LinearProgress";
-
-type CardProps = {
-  title?: string;
-  counts?: number;
-};
+import paperCardStyle from "./Card.style";
+import { CardProps } from "./CardProps";
 
 function LinearProgressWithLabel(
   props: LinearProgressProps & { value: number }
 ) {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        paddingRight: "15px",
-        paddingLeft: "15px",
-      }}
-    >
-      <Box sx={{ width: "100%", mr: 1, borderRadius: "20px" }}>
+    <Box className="boxCardContainerStyle">
+      <Box>
         <LinearProgress
           variant="determinate"
           {...props}
-          sx={{ borderRadius: "20px" }}
+          className="linearProgressStyle"
         />
       </Box>
     </Box>
@@ -34,31 +24,19 @@ function LinearProgressWithLabel(
 const Card = function ({ title, counts }: CardProps) {
   const [progress, setProgress] = React.useState(10);
   return (
-    <Paper
-      variant="outlined"
-      square
-      sx={{
-        height: "150px",
-        width: "350px",
-        background: "#fff",
-        borderRadius: "10px",
-        border: "none",
-      }}
-    >
-      <Stack flexDirection="column" sx={{ gap: "30px", padding: "20px" }}>
-        <Stack
-          flexDirection="row"
-          sx={{
-            justifyContent: "space-around",
-            alignItems: "center",
-          }}
-        >
-          <Typography sx={{ fontSize: "20px" }}>{title}</Typography>
-          <Typography sx={{ fontSize: "20px" }}>{counts}</Typography>
+    <Paper variant="outlined" square sx={paperCardStyle}>
+      <Stack flexDirection="column" className="stackColumnPaperStyle">
+        <Stack flexDirection="row" className="stackRowPaperStyle">
+          <Typography className="typographyContentPaperStyle">
+            {title}
+          </Typography>
+          <Typography className="typographyContentPaperStyle">
+            {counts}
+          </Typography>
         </Stack>
         <LinearProgressWithLabel value={progress} />
         <Stack flexDirection="row" justifyContent="space-around">
-          <Typography sx={{ fontSize: "20px" }}>-1.2%</Typography>
+          <Typography className="typographyContentPaperStyle">-1.2%</Typography>
           <Typography>از دیروز</Typography>
         </Stack>
       </Stack>
