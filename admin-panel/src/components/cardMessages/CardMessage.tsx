653dcdac83acda79066608bb521757666f2f9fee
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-12-10 23:10:43 +0330

    chore(css):Added round checkbox style.

--- admin-panel/src/components/cardMessages/CardMessage.tsx
+++ admin-panel/src/components/cardMessages/CardMessage.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Box,
   CardMedia,
@@ -9,13 +10,80 @@ import {
   ListItemAvatar,
   ListItemText,
   Avatar,
+  CheckboxProps,
+  styled,
 } from "@mui/material";
-import React from "react";
 import CardMessageStyled from "./CardMessage.style";
 
-const CardMessage = function ({ toggleOpenMessage, settings }: any) {
-  console.log("toggleOpenMessage CardMessage :::", toggleOpenMessage);
-  console.log("settings :", settings.cardType);
+const BpIcon = styled("span")(({ theme }) => ({
+  borderRadius: "50%",
+  width: 16,
+  height: 16,
+  marginBottom: "15px",
+  boxShadow:
+    theme.palette.mode === "dark"
+      ? "0 0 0 1px rgb(16 22 26 / 40%)"
+      : "inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)",
+  backgroundColor: theme.palette.mode === "dark" ? "#394b59" : "#f5f8fa",
+  backgroundImage:
+    theme.palette.mode === "dark"
+      ? "linear-gradient(180deg,hsla(0,0%,100%,.05),hsla(0,0%,100%,0))"
+      : "linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))",
+  ".Mui-focusVisible &": {
+    outline: "2px auto rgba(19,124,189,.6)",
+    outlineOffset: 2,
+  },
+  "input:hover ~ &": {
+    backgroundColor: theme.palette.mode === "dark" ? "#30404d" : "#ebf1f5",
+  },
+  "input:disabled ~ &": {
+    boxShadow: "none",
+    background:
+      theme.palette.mode === "dark"
+        ? "rgba(57,75,89,.5)"
+        : "rgba(206,217,224,.5)",
+  },
+}));
+
+const BpCheckedIcon = styled(BpIcon)({
+  // backgroundColor: "#137cbd",
+  backgroundColor: "#004deb",
+  backgroundImage:
+    "linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))",
+  "&:before": {
+    display: "block",
+    width: 16,
+    height: 16,
+    backgroundImage:
+      "url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath" +
+      " fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 " +
+      "1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
+    content: '""',
+  },
+  "input:hover ~ &": {
+    // backgroundColor: "#106ba3",
+    backgroundColor: "#407bf0",
+  },
+});
+
+// Inspired by blueprintjs
+function BpCheckbox(props: CheckboxProps) {
+  return (
+    <Checkbox
+      sx={{
+        "&:hover": { bgcolor: "transparent" },
+      }}
+      disableRipple
+      color="default"
+      checkedIcon={<BpCheckedIcon />}
+      icon={<BpIcon />}
+      inputProps={{ "aria-label": "Checkbox demo" }}
+      {...props}
+    />
+  );
+}
+
+const CardMessage = function ({ toggleMessage, settings }: any) {
   const [checked, setChecked] = React.useState([2]);
   const handleToggle = (value: number) => () => {
     const currentIndex = checked.indexOf(value);
@@ -29,7 +97,7 @@ const CardMessage = function ({ toggleOpenMessage, settings }: any) {
     setChecked(newChecked);
   };
   return (
-    <CardMessageStyled isOpen={toggleOpenMessage} cardType={settings.cardType}>
+    <CardMessageStyled isOpen={toggleMessage} cardType={settings.cardType}>
       <CardMedia
         component="div"
         className="boxImageHeaderMessageStyle"
@@ -58,8 +126,12 @@ const CardMessage = function ({ toggleOpenMessage, settings }: any) {
         }
       />
       <List
-        dense
-        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        sx={{
+          padding: "20px 10px 20px 10px",
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper",
+        }}
       >
         {[0, 1, 2, 3].map((value) => {
           const labelId = `checkbox-list-secondary-label-${value}`;
@@ -67,7 +139,7 @@ const CardMessage = function ({ toggleOpenMessage, settings }: any) {
             <ListItem
               key={value}
               secondaryAction={
-                <Checkbox
+                <BpCheckbox
                   edge="end"
                   onChange={handleToggle(value)}
                   checked={checked.indexOf(value) !== -1}
@@ -77,13 +149,22 @@ const CardMessage = function ({ toggleOpenMessage, settings }: any) {
               disablePadding
             >
               <ListItemButton>
-                <ListItemAvatar>
+                <ListItemAvatar sx={{ minWidth: "40px" }}>
                   <Avatar
+                    sx={{ width: "30px", height: "30px", marginBottom: "15px" }}
                     alt={`Avatar n°${value + 1}`}
                     src={`/static/images/avatar/${value + 1}.jpg`}
                   />
                 </ListItemAvatar>
-                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+                <ListItemText
+                  id={labelId}
+                  primary={`Line item ${value + 1}`}
+                  secondary={
+                    <Typography sx={{ fontSize: "11px", color: "#a7abc3" }}>
+                      02:30 ب.ظ
+                    </Typography>
+                  }
+                />
               </ListItemButton>
             </ListItem>
           );
