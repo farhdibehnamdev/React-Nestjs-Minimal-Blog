bae60614df6bf503aff0dc6696be8807312e5d5b
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-12-12 15:07:08 +0330

    refactor:Refactored div to Grid.

--- admin-panel/src/App.tsx
+++ admin-panel/src/App.tsx
@@ -1,16 +1,10 @@
-import React from "react";
 import Layout from "./components/layout/Layout";
 import rtlPlugin from "stylis-plugin-rtl";
 import { prefixer } from "stylis";
 import createCache from "@emotion/cache";
 import { CacheProvider } from "@emotion/react";
 import { CssBaseline, Grid, ThemeProvider } from "@mui/material";
-import theme from "./styles/theme";
-import { Provider, useDispatch, useSelector } from "react-redux";
-import {
-  toggleMessage,
-  toggleNotification,
-} from "./features/toggle/toggleSlice";
+import theme from "./globalStyles/theme";
 const cacheRTL = createCache({
   key: "muirtl",
   stylisPlugins: [prefixer, rtlPlugin],
@@ -18,14 +12,14 @@ const cacheRTL = createCache({
 
 function App() {
   return (
-    <div style={{ width: "100%" }}>
+    <Grid>
       <CacheProvider value={cacheRTL}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Layout />
         </ThemeProvider>
       </CacheProvider>
-    </div>
+    </Grid>
   );
 }
 
