8a7e86517b70faed0ba242b9c92daf2fd375f0c3
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-12-15 19:00:15 +0330

    chore(css):Hide arrow down icon in large scale.

--- admin-panel/src/components/arrowDownToggleToolbar/ArrowDownToggleToolbar.styled.ts
+++ admin-panel/src/components/arrowDownToggleToolbar/ArrowDownToggleToolbar.styled.ts
@@ -2,6 +2,9 @@ import { IconButton, styled } from "@mui/material";
 import theme from "../../globalStyles/theme";
 
 const BottomToolbarStyled = styled(IconButton)(() => ({
+  [theme.breakpoints.up("lg")]: {
+    display: "none",
+  },
   [theme.breakpoints.down("lg")]: {
     marginLeft: "auto",
     color: "#000",
