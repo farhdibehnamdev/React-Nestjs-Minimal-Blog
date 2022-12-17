c87ff5675a61026f62657495d54bb54d4c9752ad
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-12-17 21:45:54 +0330

    chore(css):Aligned toolbar's elements.

--- admin-panel/src/components/ToolbarBottom/ToolbarBottom.styled.ts
+++ admin-panel/src/components/ToolbarBottom/ToolbarBottom.styled.ts
@@ -1,10 +1,26 @@
-import { styled, Toolbar } from "@mui/material";
+import { Box, styled, Toolbar } from "@mui/material";
 
 const ToolbarBottomStyled = styled(Toolbar)(({ theme }) => ({
+  [theme.breakpoints.up("lg")]: {
+    ".gridSearchBoxStyle": {
+      display: "none",
+    },
+  },
   [theme.breakpoints.down("lg")]: {
-    padding: 0,
-    margin: 0,
+    display: "flex",
+    paddingLeft: 25,
+    paddingRight: 25,
     borderBottom: "1px solid #000",
+    borderTop: "1px solid #000",
+    margin: 0,
+    ".boxContainerMenuIconsStyle": {
+      marginLeft: "auto",
+    },
+    ".headerIconButtonStyle": {
+      display: "flex",
+      justifyContent: "space-between",
+      gap: "20px",
+    },
     ".gridSearchBoxStyle": {
       display: "block",
     },
