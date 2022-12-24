7a2b0f3ea5010ba70970839567d090bb630c669a
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-12-24 21:39:06 +0330

    chore:Added Router in root of project.

--- admin-panel/src/index.tsx
+++ admin-panel/src/index.tsx
@@ -4,15 +4,17 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./app/store";
 import reportWebVitals from "./reportWebVitals";
-
+import { BrowserRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
 
