128dd866937ce9f49f8091bcfadb2341d4d5e172
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-12-25 20:59:02 +0330

    chore:Added roles and versioning to tags and article controller.

--- server/src/article/article.controller.ts
+++ server/src/article/article.controller.ts
@@ -7,7 +7,12 @@ import {
   Patch,
   Param,
 } from '@nestjs/common';
+import { UseGuards, Version } from '@nestjs/common/decorators';
 import { PaginationQueryDto } from 'src/common/pagination-query.dto';
+import { Role } from 'src/user/decorators/role';
+import { UserRole } from 'src/user/entities/user.entity';
+import { AccessTokenGuard } from 'src/user/guard/access-token.guard';
+import { RoleGuard } from 'src/user/guard/authorization.guard';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
@@ -16,26 +21,33 @@ import { UpdateArticleDto } from './dto/update-article.dto';
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
+  @Version('1')
   @Get()
   findAll(paginationQueryDto: PaginationQueryDto) {
     return this.articleService.findAll(paginationQueryDto);
   }
-
+  @Version('1')
   @Get()
   findOne(id: number) {
     return this.articleService.findOne(id);
   }
-
+  @Version('1')
+  @Role(UserRole.ADMIN)
+  @UseGuards(AccessTokenGuard, RoleGuard)
   @Post()
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articleService.create(createArticleDto);
   }
-
+  @Version('1')
+  @Role(UserRole.ADMIN)
+  @UseGuards(AccessTokenGuard, RoleGuard)
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateArticleDto: UpdateArticleDto) {
     return this.articleService.update(id, updateArticleDto);
   }
-
+  @Version('1')
+  @Role(UserRole.ADMIN)
+  @UseGuards(AccessTokenGuard, RoleGuard)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.articleService.remove(id);
--- server/src/tag/tag.controller.ts
+++ server/src/tag/tag.controller.ts
@@ -6,34 +6,48 @@ import {
   Post,
   Param,
   Body,
+  Version,
+  UseGuards,
 } from '@nestjs/common';
+import { Role } from 'src/user/decorators/role';
+import { UserRole } from 'src/user/entities/user.entity';
+import { AccessTokenGuard } from 'src/user/guard/access-token.guard';
+import { RoleGuard } from 'src/user/guard/authorization.guard';
 import { CreateTagDto } from './dto/create-tag.dto';
 import { TagService } from './tag.service';
 
-@Controller('tag')
+@Controller('api/tag')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
 
+  @Version('1')
   @Get()
   findAll() {
     return this.tagService.findAll();
   }
-
+  @Version('1')
   @Get(':id')
   findOne(id: number) {
     return this.tagService.findOne(id);
   }
-
+  @Role(UserRole.ADMIN)
+  @UseGuards(AccessTokenGuard, RoleGuard)
+  @Version('1')
   @Post()
   create(@Body() createTagDto: CreateTagDto) {
     return this.tagService.create(createTagDto);
   }
-
+  @Role(UserRole.ADMIN)
+  @UseGuards(AccessTokenGuard, RoleGuard)
+  @Version('1')
   @Patch(':id')
   update(@Param('id') id: number, @Body() createTagDto: CreateTagDto) {
     return this.tagService.update(id, createTagDto);
   }
 
+  @Role(UserRole.ADMIN)
+  @UseGuards(AccessTokenGuard, RoleGuard)
+  @Version('1')
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.tagService.remove(id);
