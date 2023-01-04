24c71c46af055f7ce3635b18eb74883bd3d2891d
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-01-04 22:20:06 +0330

    chore:Added breadcrumb to top of posts.

--- admin-panel/src/components/posts/Posts.tsx
+++ admin-panel/src/components/posts/Posts.tsx
@@ -1,10 +1,22 @@
-import { Outlet } from "react-router-dom";
+import Grid from "@mui/material/Grid";
 import useFetchPost from "../../hooks/useFetchPost";
+import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
+import { BreadcrumbsType } from "../common/BreadcrumbsProps";
 import DataTable from "../table/DataTable";
-
+const breadcrumbTitles: BreadcrumbsType = {
+  titles: ["داشبورد", "پست ها"],
+};
 const Posts = function () {
   const { post } = useFetchPost();
 
-  return <DataTable />;
+  return (
+    <>
+      <Grid item mb={5.2}>
+        <h1>پست ها</h1>
+        <Breadcrumbs {...breadcrumbTitles} />
+      </Grid>
+      <DataTable />
+    </>
+  );
 };
 export default Posts;
