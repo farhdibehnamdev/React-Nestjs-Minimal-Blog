9835054531e497f0db4d57eee28dcb5b25bfd850
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-01-06 23:13:37 +0330

    chore:Added categoryReducer in configureStore.

--- admin-panel/src/app/store.ts
+++ admin-panel/src/app/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import categorySlice from "../features/category/categorySlice";
+import { postSlice } from "../features/post/postSlice";
 import { toggleSlice } from "../features/toggle/toggleSlice";
 
 const store = configureStore({
   reducer: {
     toggle: toggleSlice.reducer,
+    post: postSlice.reducer,
+    category: categorySlice.reducer,
   },
 });
 
