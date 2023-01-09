ca596ec8ad8ea47423c9f91f9d6c726ab7d6e5a1
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-01-09 22:31:05 +0330

    feat:Added breadcrumb in home component.

--- admin-panel/src/components/home/Home.tsx
+++ admin-panel/src/components/home/Home.tsx
@@ -1,9 +1,18 @@
-import { Stack } from "@mui/material";
+import { Grid, Stack } from "@mui/material";
+import Box from "@mui/material/Box";
 import Card from "../card/Card";
-
+import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
+import { BreadcrumbsType } from "../common/BreadcrumbsProps";
+const breadcrumbTitles: BreadcrumbsType = {
+  titles: ["داشبورد"],
+};
 const Home = function () {
   return (
     <>
+      <Grid item mb={5.2}>
+        <h1>داشبورد ها</h1>
+        <Breadcrumbs {...breadcrumbTitles} />
+      </Grid>
       <Stack flexDirection="row" className="stackStyleWrapperCards">
         <Card title="تعداد پست ها" counts={10} />
         <Card title="تعداد فهرست ها" counts={30} />
