d8b148a369fb65a548fe1401b1a8f49c47f485ae
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-01-20 23:04:46 +0330

    feat:Added useEffect to fetch tags data.

--- admin-panel/src/components/tags/Tags.tsx
+++ admin-panel/src/components/tags/Tags.tsx
@@ -1,25 +1,29 @@
-import Box from "@mui/material/Box";
+import { Grid } from "@mui/material";
 import DataTable from "../table/DataTable";
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 import { BreadcrumbsType } from "../common/BreadcrumbsProps";
+import { fetchTags } from "../../store/thunks/tagThunks/fetchTags";
+import useThunk from "../../hooks/useThunk";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 const breadcrumbTitles: BreadcrumbsType = {
   titles: ["تگ"],
 };
 const Tags = function () {
+  const [doFetchTags, isLoading, loadingTagsError] = useThunk(fetchTags);
+  const { data } = useSelector((state: any) => state.tags);
+  useEffect(() => {
+    if (typeof doFetchTags === "function") {
+      doFetchTags();
+    }
+  }, [doFetchTags]);
   return (
     <>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          marginBottom: "50px",
-        }}
-      >
+      <Grid flexDirection="column" mb={6.2}>
         <h1>تگ ها</h1>
         <Breadcrumbs {...breadcrumbTitles} />
-      </Box>
-      <DataTable />
+      </Grid>
+      <DataTable data={data} />
     </>
   );
 };
