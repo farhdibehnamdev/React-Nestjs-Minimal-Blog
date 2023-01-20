e5430c24ccca9313fbe435e65444a5bf8a07fd89
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-01-20 22:59:34 +0330

    chore:Added body in post,patch,put parameters.

--- admin-panel/src/config/api/api.ts
+++ admin-panel/src/config/api/api.ts
@@ -13,12 +13,12 @@ const api = (axios: AxiosInstance) => {
       axios.get<T>(url, config),
     delete: <T>(url: string, config: AxiosRequestConfig = {}) =>
       axios.delete<T>(url, config),
-    post: <T>(url: string, config: AxiosRequestConfig = {}) =>
-      axios.post<T>(url, config),
-    patch: <T>(url: string, config: AxiosRequestConfig = {}) =>
-      axios.patch<T>(url, config),
-    put: <T>(url: string, config: AxiosRequestConfig = {}) =>
-      axios.put<T>(url, config),
+    post: <T>(url: string, body: unknown, config: AxiosRequestConfig = {}) =>
+      axios.post<T>(url, body, config),
+    patch: <T>(url: string, body: unknown, config: AxiosRequestConfig = {}) =>
+      axios.patch<T>(url, body, config),
+    put: <T>(url: string, body: unknown, config: AxiosRequestConfig = {}) =>
+      axios.put<T>(url, body, config),
   };
 };
 export default api(axiosInstance);
