960055029ad538f6a36223d1e8e393589d650ea8
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-01-21 23:42:53 +0330

    chore:Defined DataTable's properties.

---
+++ admin-panel/src/components/table/DataTable.type.ts
@@ -0,0 +1,14 @@
+export interface IDataTableColumn {
+  id: string;
+  title: string;
+  align?: "center" | "justify" | "left" | "right";
+}
+
+export interface IDataTableHeadProps {
+  columns: IDataTableColumn[];
+}
+
+export interface IDataTableProps {
+  rows: [];
+  columnData?: IDataTableColumn[];
+}
