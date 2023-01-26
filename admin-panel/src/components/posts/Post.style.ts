b35fa8ca128679160ed894125ff96daa85930aa8
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-01-26 18:17:31 +0330

    remove unnecessary style from post's style.

--- admin-panel/src/components/posts/Post.style.ts
+++ admin-panel/src/components/posts/Post.style.ts
@@ -1,13 +1,1 @@
-import { SxProps } from "@mui/material";
-const addPostStyle: SxProps = {
-  //   marginBottom: "20px",
-  background: "#fff",
-  padding: "40px",
-  borderRadius: "10px",
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  marginBottom: "50px",
-};
-
-export default addPostStyle;
+export {};
