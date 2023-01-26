1593bbcff3af1faafce0a4ce22460964e2d478b0
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-01-26 18:13:17 +0330

    fix:Fixed pagination that didn't go to next page.

--- admin-panel/src/components/table/DataTable.tsx
+++ admin-panel/src/components/table/DataTable.tsx
@@ -15,7 +15,33 @@ import DataTableHead from "./DataTableHead";
 import DataTableBody from "./DataTableBody";
 import { Link } from "react-router-dom";
 import TablePagination from "./TablePagination";
-const DataTable = function ({ columns, rows, typeOperation }: any) {
+import usePagination from "src/hooks/usePagination";
+import { useEffect, useState } from "react";
+import useThunk from "src/hooks/useThunk";
+import { fetchTags } from "src/store/thunks/tagThunks/fetchTags";
+const DataTable = function ({ columns, rows, count, typeOperation }: any) {
+  const [offset, setOffset] = useState<number>(1);
+  const [perPage, setPerPage] = useState<number>(10);
+  const [doFetchTags, isLoading, loadingTagsError] = useThunk(fetchTags);
+  const pageCount = Math.ceil(count / perPage);
+  const _DATA = usePagination(rows, perPage);
+
+  const handleChange = function (page: number = offset) {
+    setOffset(page);
+    if (typeof doFetchTags === "function") {
+      doFetchTags({ offset: page, limit: perPage });
+    }
+    _DATA.jump(page);
+  };
+
+  const handleChangeRowCount = function (rowCount: number = perPage) {
+    setOffset(1);
+    setPerPage(rowCount);
+    if (typeof doFetchTags === "function") {
+      doFetchTags({ offset: 1, limit: rowCount });
+    }
+    _DATA.jump(1);
+  };
   return (
     <>
       <Box sx={filterTable}>
@@ -64,10 +90,21 @@ const DataTable = function ({ columns, rows, typeOperation }: any) {
           aria-label="simple table"
         >
           <DataTableHead columns={columns} />
-          <DataTableBody rows={rows} />
+          <DataTableBody
+            rows={_DATA.currentData()}
+            offset={offset}
+            perPage={perPage}
+          />
         </TableMui>
       </TableContainer>
-      <TablePagination />
+      <TablePagination
+        count={pageCount}
+        handleChange={handleChange}
+        setPerPage={setPerPage}
+        page={offset}
+        perPage={perPage}
+        handleChangeRowCount={handleChangeRowCount}
+      />
     </>
   );
 };
