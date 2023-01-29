4e31f5f079fc853aaa00468049d56f2a64b7159f
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-01-29 23:46:31 +0330

    refactor:Added pagination parameter for fetching tags.

--- admin-panel/src/store/thunks/tagThunks/fetchTags.ts
+++ admin-panel/src/store/thunks/tagThunks/fetchTags.ts
@@ -1,9 +1,15 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { fetchTagsData } from "../../../config/api/tagsApi/tagsApi";
-
-const fetchTags = createAsyncThunk("tags/fetch", async () => {
-  const response = await fetchTagsData();
-  return response.data;
-});
+import { fetchTagsData } from "src/config/api/tagsApi/tagsApi";
+type paginationOptionType = {
+  offset: number;
+  limit: number;
+};
+const fetchTags = createAsyncThunk(
+  "tags/fetch",
+  async (pagination: paginationOptionType) => {
+    const response = await fetchTagsData(pagination);
+    return response.data;
+  }
+);
 
 export { fetchTags };
