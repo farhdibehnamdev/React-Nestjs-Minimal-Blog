167f1722de094ac8f2b650cea38e4a139c543dff
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-01-31 23:34:28 +0330

    Fix:Fixed after deleting item back to previous page.

--- admin-panel/src/components/table/DataTable.tsx
+++ admin-panel/src/components/table/DataTable.tsx
@@ -1,103 +1,80 @@
-import {
-  Paper,
-  TableContainer,
-  Table,
-  Box,
-  Button,
-  Typography,
-  TextField,
-  Grid,
-} from "@mui/material";
-import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
-import dataTableMUI, {
-  filterTable,
-  tableContainerStyle,
-} from "./DataTable.style";
-import InputAdornment from "@mui/material/InputAdornment";
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { TableContainer, Table, Box, Grid } from "@mui/material";
+import dataTableMUI, { tableContainerStyle } from "./DataTable.style";
 import DataTableHead from "./DataTableHead";
 import DataTableBody from "./DataTableBody";
-import { Link } from "react-router-dom";
 import TablePagination from "./TablePagination";
 import usePagination from "src/hooks/usePagination";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useThunk from "src/hooks/useThunk";
 import { fetchTags } from "src/store/thunks/tagThunks/fetchTags";
-const DataTable = function ({ columns, rows, count, typeOperation }: any) {
+import CircularProgress from "@mui/material/CircularProgress";
+import { useSelector } from "react-redux";
+import FilterTable from "./FilterTable";
+const DataTable = function ({
+  columns,
+  rows,
+  count,
+  typeOperation,
+  thunkFunction,
+}: any) {
   const [offset, setOffset] = useState<number>(1);
-  const [perPage, setPerPage] = useState<number>(10);
-  const [doFetchTags, isLoading, loadingTagsError] = useThunk(fetchTags);
-  const pageCount = Math.ceil(count / perPage);
+  const [perPage, setPerPage] = useState<number>(5);
+  const { isLoading } = useSelector((state: any) => state.tags);
+  const [doFetchTags] = useThunk(fetchTags);
+  const pageNumber = Math.ceil(count / perPage);
   const _DATA = usePagination(rows, perPage);
-
-  const handleChange = function (page: number = offset) {
+  const handleChange = async function (page: number = offset) {
     setOffset(page);
-    if (typeof doFetchTags === "function") {
-      doFetchTags({ offset: page, limit: perPage });
-    }
+    await doFetchTags({ offset: page, limit: perPage });
     _DATA.jump(page);
   };
-
   const handleChangeRowCount = function (rowCount: number = perPage) {
     setOffset(1);
     setPerPage(rowCount);
-    if (typeof doFetchTags === "function") {
-      doFetchTags({ offset: 1, limit: rowCount });
-    }
+    doFetchTags({ offset: 1, limit: rowCount });
     _DATA.jump(1);
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (rows.length < 1) {
+        setOffset(pageNumber);
+        if (offset > 0) {
+          await doFetchTags({ offset, limit: perPage });
+        }
+      }
+    };
+    fetchData();
+  }, [pageNumber, offset, rows, doFetchTags, perPage]);
   return (
     <>
-      <Box sx={filterTable}>
-        <Link to="add" style={{ textDecoration: "none" }}>
-          <Button
-            variant="contained"
-            size="large"
-            startIcon={<AddOutlinedIcon />}
-          >
-            <Typography sx={{ color: "#fff" }}>
-              افزودن {typeOperation}
-            </Typography>
-          </Button>
-        </Link>
-
-        <Box>
-          <TextField
-            id="standard-search"
-            placeholder="جستجو"
-            type="search"
-            variant="outlined"
-            hiddenLabel
-            InputLabelProps={{
-              disableAnimation: true,
-              shrink: false,
-            }}
-            className="textFieldFilterStyle"
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="start">
-                  <SearchOutlinedIcon />
-                </InputAdornment>
-              ),
-              className: "filterTableHeaderInputPropsStyle",
-            }}
-          />
-        </Box>
-      </Box>
+      <FilterTable typeOperation={typeOperation} />
       <Grid sx={dataTableMUI}>
         <TableContainer sx={tableContainerStyle}>
           <Table stickyHeader aria-label="sticky table">
             <DataTableHead columns={columns} />
-            <DataTableBody
-              rows={_DATA.currentData()}
-              offset={offset}
-              perPage={perPage}
-            />
+            {isLoading ? (
+              <Box
+                style={{
+                  position: "absolute",
+                  left: "50%",
+                  bottom: "40%",
+                }}
+              >
+                <CircularProgress sx={{ color: "#8d8d91" }} />
+              </Box>
+            ) : (
+              <DataTableBody
+                rows={_DATA.currentData()}
+                offset={offset}
+                perPage={perPage}
+              />
+            )}
           </Table>
         </TableContainer>
       </Grid>
       <TablePagination
-        count={pageCount}
+        count={pageNumber}
         handleChange={handleChange}
         setPerPage={setPerPage}
         page={offset}
