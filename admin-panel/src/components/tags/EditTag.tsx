693b1c5bd29efb6b3166cbb4715f48a19a207012
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-02-03 23:48:11 +0330

    chore:Added the logic for selecting tag and send to AddEdit component.

--- admin-panel/src/components/tags/EditTag.tsx
+++ admin-panel/src/components/tags/EditTag.tsx
@@ -2,24 +2,81 @@ import AddEditTag from "./AddEditTag";
 import { BreadcrumbsType } from "../common/BreadcrumbsProps";
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 import Box from "@mui/material/Box";
+import useThunk from "src/hooks/useThunk";
+import { editTag } from "src/store/thunks/tagThunks/editTag";
+import React, { useEffect, useState } from "react";
+import { editTagType } from "src/config/api/tagsApi/tagsApi";
+import Grid from "@mui/material/Grid";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+import { useParams } from "react-router-dom";
+import { useAppSelector } from "src/store/hooks";
+import { selectTagById } from "src/store/slices/tag/tagSlice";
+
 const breadcrumbTitles: BreadcrumbsType = {
   titles: ["تگ", "ویرایش تگ"],
 };
 const EditTag = function () {
+  const { id } = useParams();
+  const [editTagInfo, isEditingTag, editingTagError] = useThunk(editTag);
+  const tag = useAppSelector((state) =>
+    selectTagById(state, parseInt(id as string))
+  );
+  const [open, setOpen] = useState<boolean>(false);
+  const handleTagEdit = function (formData: editTagType) {
+    editTagInfo(formData);
+    setOpen(true);
+  };
+
+  const handleClose = function () {
+    setOpen(false);
+  };
   return (
     <>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          marginBottom: "50px",
-        }}
-      >
+      {!!editingTagError ? (
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          open={open}
+          autoHideDuration={3000}
+          onClose={handleClose}
+        >
+          <Alert
+            onClose={handleClose}
+            variant="filled"
+            severity="error"
+            sx={{ width: "100%" }}
+          >
+            مشکلی در ارسال دیتا بوجود آمده است
+          </Alert>
+        </Snackbar>
+      ) : (
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          open={open}
+          autoHideDuration={6000}
+          onClose={handleClose}
+        >
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            تگ با موفقیت ویرایش شد
+          </Alert>
+        </Snackbar>
+      )}
+      <Grid item mb={5.2}>
         <h1>ویرایش تگ</h1>
         <Breadcrumbs {...breadcrumbTitles} />
-      </Box>
-      <AddEditTag typeOperation="Edit" />
+      </Grid>
+      {tag && (
+        <AddEditTag
+          typeOperation="Edit"
+          onEdit={handleTagEdit}
+          editFormData={tag}
+        />
+      )}
     </>
   );
 };
