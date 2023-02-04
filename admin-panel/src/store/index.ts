1ce1194b2b03cd006cfd1b3972188aa34e326ef5
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-02-04 23:34:46 +0330

    chore:Defined useAppSelctor and useAppDispatch preventing prevent some errors later

--- admin-panel/src/store/index.ts
+++ admin-panel/src/store/index.ts
@@ -1,16 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categorySlice from "./slices/category/categorySlice";
+import { modalReducer } from "./slices/modal/modalSlice";
 import { postSlice } from "./slices/post/postSlice";
 import { tagReducer } from "./slices/tag/tagSlice";
 import { toggleSlice } from "./slices/toggle/toggleSlice";
-
 const store = configureStore({
   reducer: {
     toggle: toggleSlice.reducer,
     post: postSlice.reducer,
     category: categorySlice.reducer,
     tags: tagReducer,
+    modal: modalReducer,
   },
 });
 
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 export default store;
