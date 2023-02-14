4f38505c988f137f1c68f2fadd62ee34832ae586
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-02-14 23:17:45 +0330

    fix:Fixed editting tag by adding id.

--- admin-panel/src/store/thunks/tagThunks/editTag.ts
+++ admin-panel/src/store/thunks/tagThunks/editTag.ts
@@ -2,8 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { editTagData, editTagType } from "src/config/api/tagsApi/tagsApi";
 
 const editTag = createAsyncThunk("tag/edit", async (tag: editTagType) => {
-  const response = await editTagData(tag);
-
+  const response = await editTagData(tag.id, tag);
   return response.data;
 });
 
