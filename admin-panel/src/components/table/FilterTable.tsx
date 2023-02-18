74f8e6c0a256c334501f99981f89cfac85594c51
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-02-18 22:51:55 +0330

    fix:Fixed filtering in DataTable by set null in setFilterData

--- admin-panel/src/components/table/FilterTable.tsx
+++ admin-panel/src/components/table/FilterTable.tsx
@@ -1,15 +1,14 @@
-import React, { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Box, Button, Typography, TextField } from "@mui/material";
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 import { filterTable } from "./DataTable.style";
 import { Link } from "react-router-dom";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import axios from "axios";
-import {
-  fetchFiltersData,
-  fetchTagsData,
-} from "src/config/api/tagsApi/tagsApi";
+import { fetchFiltersData } from "src/config/api/tagsApi/tagsApi";
+import { useAppSelector } from "src/store/hooks";
+import { fetchTags } from "src/store/thunks/tagThunks/fetchTags";
+import useThunk from "src/hooks/useThunk";
 
 const FilterTable = function ({
   typeOperation,
@@ -17,17 +16,27 @@ const FilterTable = function ({
   perPage,
   offset,
 }: any) {
-  const [searchTerm, setSearchTerm] = useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>();
+  const { data } = useAppSelector((state) => state.tags);
+  const [doFetchTags] = useThunk(fetchTags);
   const handleSearch = function (e: any) {
     setSearchTerm(e.target.value);
   };
   useEffect(() => {
-    const pagination = { offset: 0, limit: perPage };
-    if (searchTerm === "" || searchTerm) {
-      fetchFiltersData(pagination, searchTerm).then((res) =>
-        setFilterData(res.data.data)
-      );
-    }
+    const fetchFilter = async () => {
+      const title = searchTerm || undefined;
+      if (searchTerm) {
+        const pagination = { offset: 1, limit: perPage };
+        fetchFiltersData(pagination, title).then((res) =>
+          setFilterData(res.data.data)
+        );
+      } else if (searchTerm === "") {
+        const pagination = { offset, limit: perPage };
+        await doFetchTags(pagination);
+        setFilterData(null);
+      }
+    };
+    fetchFilter();
   }, [searchTerm]);
   return (
     <Box sx={filterTable}>
