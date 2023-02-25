7efd4ace3db27d5435d021fa8e20eba534efe072
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-02-25 23:09:48 +0330

    chore:if T as a Article i should add relations to findAndCounts config.

--- server/src/common/base.service.ts
+++ server/src/common/base.service.ts
@@ -2,26 +2,35 @@ import { HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
 import { PaginationQueryDto } from './pagination-query.dto';
 import {
   Brackets,
+  FindManyOptions,
   FindOperator,
   FindOptionsWhere,
   Like,
   Repository,
   SelectQueryBuilder,
 } from 'typeorm';
+import Article from 'src/article/entities/article.entity';
 
 export class BaseService<T> {
   constructor(private readonly repository: Repository<T>) {}
+  isArticle(target: any): target is Article {
+    return target && target.prototype instanceof Article;
+  }
 
   async paginate(paginationQuery: PaginationQueryDto) {
     try {
       const { limit, offset } = paginationQuery;
-      console.log('my offset:', offset);
       const newSkip = offset <= 1 ? 0 : (offset - 1) * limit;
-      console.log('newSkip:', newSkip);
-      const [items, total] = await this.repository.findAndCount({
+
+      let options: FindManyOptions = {
         skip: newSkip,
         take: limit,
-      });
+      };
+
+      if (this.isArticle(this.repository.target)) {
+        options.relations = ['category', 'tags'];
+      }
+      const [items, total] = await this.repository.findAndCount(options);
       return { data: items, count: total };
     } catch (error) {
       throw new HttpException(
@@ -36,15 +45,19 @@ export class BaseService<T> {
     searchCriteria: FindOptionsWhere<T>,
   ) {
     const { limit, offset } = paginationQuery;
-    // const newSkip = offset * limit;
     const newSkip = offset <= 1 ? 0 : (offset - 1) * limit;
 
-    const [items, total] = await this.repository.findAndCount({
+    let options: FindManyOptions = {
       skip: newSkip,
       take: limit,
-      //   where: [{ title: Like(`%${tagTitle}%`) }],
       where: searchCriteria,
-    });
+    };
+
+    if (this.isArticle(this.repository.target)) {
+      options.relations = ['category', 'tags'];
+    }
+    const [items, total] = await this.repository.findAndCount(options);
+
     if (!items) throw new NotFoundException("There isn't any Tags!!");
     return { data: items, count: total };
   }
