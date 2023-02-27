37b633076d72ba82ced1a90f1bce25ec14237eeb
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-02-27 21:23:21 +0330

    chore:Implemented post table's logic.

--- admin-panel/src/components/posts/Posts.tsx
+++ admin-panel/src/components/posts/Posts.tsx
@@ -1,18 +1,107 @@
 import Grid from "@mui/material/Grid";
+import { useEffect } from "react";
+import { fetchPosts } from "src/config/api/postsApi/postsApi";
+import useThunk from "src/hooks/useThunk";
+import { RootState } from "src/store";
+import { useAppSelector } from "src/store/hooks";
+import { fetchPostsThunk } from "src/store/thunks/postThunks/fetchPosts";
+import { removeTag } from "src/store/thunks/tagThunks/removeTag";
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 import { BreadcrumbsType } from "../common/BreadcrumbsProps";
 import DataTable from "../table/DataTable";
 const breadcrumbTitles: BreadcrumbsType = {
   titles: ["داشبورد", "پست ها"],
 };
+const columns = [
+  {
+    colId: 1,
+    field: "colId",
+    headerName: "ردیف",
+    width: 20,
+  },
+  {
+    colId: 2,
+    field: "title",
+    headerName: "عنوان",
+    width: 180,
+  },
+  {
+    colId: 3,
+    field: "isPublished",
+    headerName: "وضعیت",
+    width: 20,
+  },
+  {
+    colId: 4,
+    field: "publishedAt",
+    headerName: "تاریخ انتشار",
+    width: 150,
+  },
+  {
+    colId: 5,
+    field: "updatedAt",
+    headerName: "تاریخ بروزرسانی",
+    width: 150,
+  },
+  {
+    colId: 6,
+    field: "views",
+    width: 120,
+    headerName: "تعداد بازدید",
+  },
+  {
+    colId: 7,
+    field: "likes",
+    width: 120,
+    headerName: "تعداد لایک ها",
+  },
+  {
+    colId: 8,
+    field: "slug",
+    width: 120,
+    headerName: "عنوان آدرس",
+  },
+  {
+    colId: 9,
+    field: "mainImageUrl",
+    width: 20,
+    headerName: "تصویر",
+  },
+  {
+    colId: 10,
+    field: "body",
+    width: 10,
+    headerName: "متن مقاله",
+  },
+  {
+    colId: 11,
+    field: "operation",
+    headerName: "ویرایش / حذف",
+  },
+];
 const Posts = function () {
+  const postsDataSelector = (state: RootState) => state.post;
+  const [doFetchPosts] = useThunk(fetchPostsThunk);
+  const { data, count } = useAppSelector((state) => state.post);
+
+  useEffect(() => {
+    doFetchPosts();
+  }, [doFetchPosts]);
   return (
     <>
       <Grid item mb={5.2}>
         <h1>پست ها</h1>
         <Breadcrumbs {...breadcrumbTitles} />
       </Grid>
-      <DataTable />
+      <DataTable
+        count={count}
+        rows={data}
+        columns={columns}
+        thunkFetch={fetchPosts}
+        thunkRemove={removeTag}
+        typeOperation="پست ها"
+        dataSelector={postsDataSelector}
+      />
     </>
   );
 };
