59b24bbfd1d44f079f633b665048575e688bfc8a
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-03-04 22:10:37 +0330

    chore:Implemented addPostThunk function.

---
+++ admin-panel/src/store/thunks/postThunks/addPostThunk.ts
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { addPost } from "src/config/api/postsApi/postsApi";
+type postType = {
+  id: number;
+  title: string;
+  body: string;
+  isPublished: boolean;
+  publishedAt: string;
+  mainImageUrl: string;
+  categoryId: number;
+  userId: string;
+  tags: string[];
+};
+const addPostThunk = createAsyncThunk("post/addPost", async (arg: postType) => {
+  const response = await addPost(arg);
+  return response.data;
+});
+
+export { addPostThunk };
