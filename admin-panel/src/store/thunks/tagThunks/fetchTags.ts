a7b856dc9022cbedf3e871bffdca09e78229269f
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-03-04 22:10:00 +0330

    refactor:Refactored structure of fetchTags based on pagination and title.

--- admin-panel/src/store/thunks/tagThunks/fetchTags.ts
+++ admin-panel/src/store/thunks/tagThunks/fetchTags.ts
@@ -2,12 +2,21 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { fetchTagsData } from "src/config/api/tagsApi/tagsApi";
 import { paginationOptionType } from "src/config/constants";
 
-const fetchTags = createAsyncThunk(
-  "tags/fetch",
-  async (pagination: paginationOptionType) => {
-    const response = await fetchTagsData(pagination);
-    return response.data;
+type fetchTagsType = {
+  pagination: paginationOptionType;
+  title: string;
+};
+
+const fetchTags = createAsyncThunk("tags/fetch", async (arg: any) => {
+  let response: undefined | any[any];
+
+  if (arg) {
+    response = await fetchTagsData(arg.pagination, arg.title);
+  } else {
+    response = await fetchTagsData();
   }
-);
+
+  return response.data;
+});
 
 export { fetchTags };
