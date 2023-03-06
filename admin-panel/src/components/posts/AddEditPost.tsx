257813ac4694332d4b44e9f196be9d6153219f47
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-03-06 22:05:43 +0330

    chore:i got bug in fill category data.

--- admin-panel/src/components/posts/AddEditPost.tsx
+++ admin-panel/src/components/posts/AddEditPost.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   TextField,
@@ -13,28 +13,81 @@ import {
   Button,
 } from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import addEditFormStyle from "../common/styles/addEditForm.style";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Uploader from "../uploader/Uploader";
 import RichTextEditor from "../richTextEditor/RichTextEditor";
 import DoDisturbAltOutlinedIcon from "@mui/icons-material/DoDisturbAltOutlined";
 import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
+import * as yup from "yup";
+import { formPostValidationType } from "./Post.type";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useAppSelector } from "src/store/hooks";
 
-const AddEditPost = function () {
-  const [age, setAge] = useState("");
+const schema = yup.object({
+  title: yup.string().required(),
+  isPublished: yup.boolean().notRequired(),
+  body: yup.string().required(),
+  publishedAt: yup.string().required(),
+  mainImageUrl: yup.string().required(),
+  userId: yup.string().required(),
+  tags: yup.string().required(),
+});
+const AddEditPost = function ({
+  typeOperation,
+  onAdd,
+  onEdit,
+  editFormData,
+}: any) {
+  const initialState = {
+    isPublished: editFormData?.isPublished || true,
+    title: editFormData?.title || "",
+    body: editFormData?.body || "",
+    mainImageUrl: editFormData?.mainImageUrl || "",
+    publishedAt: editFormData?.publishedAt || "",
+    userId: editFormData?.userId || "",
+    tags: editFormData?.tags || [],
+  };
+  const { data } = useAppSelector((state) => state.category);
   const {
-    register,
+    control,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm();
-  const onSumbit = function (data: any) {
-    console.log("data:", data);
+    reset,
+  } = useForm<formPostValidationType>({
+    resolver: yupResolver(schema),
+    defaultValues: initialState,
+  });
+  const [form, setForm] = useState(initialState);
+  const [category, setCategory] = useState("");
+  const onSumbit = function () {
+    typeOperation === "Add" ? onAdd(form) : onEdit(form);
   };
-  const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((state) => ({
+      ...state,
+      [event.target.name]:
+        event.target.name === "isPublished"
+          ? event.target.checked
+          : event.target.value,
+    }));
   };
+
+  const handleSelectChange = (event: SelectChangeEvent<any>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setForm((state) => ({
+      ...state,
+      [name]: value,
+    }));
+    console.log("set from ::", form);
+    setCategory(value);
+  };
+
+  useEffect(() => {
+    console.log("data ::", data);
+  }, [data]);
 
   return (
     <>
@@ -45,53 +98,64 @@ const AddEditPost = function () {
         >
           <Grid container mb={3} spacing={2}>
             <Grid item xl={6}>
-              <TextField
-                fullWidth
-                required
-                id="outlined-required"
-                label="عنوان پست"
-                hiddenLabel={true}
-                placeholder="عنوان پست"
-                sx={{ marginRight: "10px" }}
+              <Controller
+                control={control}
+                name="title"
+                render={({ field: { onChange, ...field } }) => (
+                  <TextField
+                    {...field}
+                    ref={null}
+                    id="title"
+                    fullWidth
+                    required
+                    label="عنوان پست"
+                    hiddenLabel={true}
+                    placeholder="عنوان پست"
+                    error={!!errors.title}
+                    helperText={errors.title ? "فیلد عنوان اجباری است" : null}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      onChange(e);
+                      handleChange(e);
+                    }}
+                  />
+                )}
               />
             </Grid>
             <Grid item xl={6}>
               <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-helper-label">
-                  فهرست
-                </InputLabel>
+                <InputLabel id="category">فهرست</InputLabel>
                 <Select
-                  labelId="demo-simple-select-helper-label"
-                  id="demo-simple-select-helper"
-                  value={age}
-                  label="Age"
-                  onChange={handleChange}
+                  labelId="categoryLabel"
+                  id="category"
+                  value={category}
+                  label="فهرست"
+                  onChange={handleSelectChange}
                 >
                   <MenuItem value="">
-                    <em>None</em>
+                    <em>هیچکدام</em>
                   </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
+                  {data.map((dt: any) => (
+                    <MenuItem value={dt.id}>{dt.title}</MenuItem>
+                  ))}
                 </Select>
               </FormControl>
             </Grid>
           </Grid>
           <Grid container mb={3} spacing={2}>
             <Grid item xl={6}>
-              <Autocomplete
+              {/* <Autocomplete
                 multiple
                 limitTags={2}
                 fullWidth
                 sx={{ marginRight: "10px" }}
                 id="multiple-limit-tags"
-                options={top100Films}
-                getOptionLabel={(option) => option.title}
-                defaultValue={[
-                  top100Films[13],
-                  top100Films[12],
-                  top100Films[11],
-                ]}
+                // options={top100Films}
+                // getOptionLabel={(option) => option.title}
+                // defaultValue={[
+                //   top100Films[13],
+                //   top100Films[12],
+                //   top100Films[11],
+                // ]}
                 renderInput={(params) => (
                   <TextField
                     {...params}
@@ -99,7 +163,7 @@ const AddEditPost = function () {
                     placeholder="Favorites"
                   />
                 )}
-              />
+              /> */}
             </Grid>
             <Grid item xl={6}>
               <FormControl
@@ -123,167 +187,44 @@ const AddEditPost = function () {
           </Grid>
           <Uploader />
           <br />
+          <RichTextEditor
+            handleChange={handleChange}
+            desc="description"
+            Controller={Controller}
+            control={control}
+          />
           <br />
-          <RichTextEditor />
-          <br />
-          <br />
-          <Grid container spacing={2} justifyContent="center">
-            <Grid item xl={6}>
-              <Button
-                fullWidth
-                variant="contained"
-                size="large"
-                startIcon={<SaveOutlinedIcon />}
-              >
-                <Typography sx={{ color: "#fff", fontSize: "20px" }}>
-                  ذخیره
-                </Typography>
-              </Button>
-            </Grid>
-            <Grid item xl={6}>
-              <Button
-                color="error"
-                fullWidth
-                variant="contained"
-                size="large"
-                startIcon={<DoDisturbAltOutlinedIcon />}
-              >
-                <Typography sx={{ color: "#fff", fontSize: "20px" }}>
-                  بازگشت
-                </Typography>
-              </Button>
-            </Grid>
-          </Grid>
         </form>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xl={6}>
+            <Button
+              fullWidth
+              variant="contained"
+              size="large"
+              startIcon={<SaveOutlinedIcon />}
+            >
+              <Typography sx={{ color: "#fff", fontSize: "20px" }}>
+                ذخیره
+              </Typography>
+            </Button>
+          </Grid>
+          <Grid item xl={6}>
+            <Button
+              color="error"
+              fullWidth
+              variant="contained"
+              size="large"
+              startIcon={<DoDisturbAltOutlinedIcon />}
+            >
+              <Typography sx={{ color: "#fff", fontSize: "20px" }}>
+                بازگشت
+              </Typography>
+            </Button>
+          </Grid>
+        </Grid>
       </Grid>
     </>
   );
 };
-const top100Films = [
-  { title: "The Shawshank Redemption", year: 1994 },
-  { title: "The Godfather", year: 1972 },
-  { title: "The Godfather: Part II", year: 1974 },
-  { title: "The Dark Knight", year: 2008 },
-  { title: "12 Angry Men", year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: "Pulp Fiction", year: 1994 },
-  {
-    title: "The Lord of the Rings: The Return of the King",
-    year: 2003,
-  },
-  { title: "The Good, the Bad and the Ugly", year: 1966 },
-  { title: "Fight Club", year: 1999 },
-  {
-    title: "The Lord of the Rings: The Fellowship of the Ring",
-    year: 2001,
-  },
-  {
-    title: "Star Wars: Episode V - The Empire Strikes Back",
-    year: 1980,
-  },
-  { title: "Forrest Gump", year: 1994 },
-  { title: "Inception", year: 2010 },
-  {
-    title: "The Lord of the Rings: The Two Towers",
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: "Goodfellas", year: 1990 },
-  { title: "The Matrix", year: 1999 },
-  { title: "Seven Samurai", year: 1954 },
-  {
-    title: "Star Wars: Episode IV - A New Hope",
-    year: 1977,
-  },
-  { title: "City of God", year: 2002 },
-  { title: "Se7en", year: 1995 },
-  { title: "The Silence of the Lambs", year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: "Life Is Beautiful", year: 1997 },
-  { title: "The Usual Suspects", year: 1995 },
-  { title: "Léon: The Professional", year: 1994 },
-  { title: "Spirited Away", year: 2001 },
-  { title: "Saving Private Ryan", year: 1998 },
-  { title: "Once Upon a Time in the West", year: 1968 },
-  { title: "American History X", year: 1998 },
-  { title: "Interstellar", year: 2014 },
-  { title: "Casablanca", year: 1942 },
-  { title: "City Lights", year: 1931 },
-  { title: "Psycho", year: 1960 },
-  { title: "The Green Mile", year: 1999 },
-  { title: "The Intouchables", year: 2011 },
-  { title: "Modern Times", year: 1936 },
-  { title: "Raiders of the Lost Ark", year: 1981 },
-  { title: "Rear Window", year: 1954 },
-  { title: "The Pianist", year: 2002 },
-  { title: "The Departed", year: 2006 },
-  { title: "Terminator 2: Judgment Day", year: 1991 },
-  { title: "Back to the Future", year: 1985 },
-  { title: "Whiplash", year: 2014 },
-  { title: "Gladiator", year: 2000 },
-  { title: "Memento", year: 2000 },
-  { title: "The Prestige", year: 2006 },
-  { title: "The Lion King", year: 1994 },
-  { title: "Apocalypse Now", year: 1979 },
-  { title: "Alien", year: 1979 },
-  { title: "Sunset Boulevard", year: 1950 },
-  {
-    title:
-      "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
-    year: 1964,
-  },
-  { title: "The Great Dictator", year: 1940 },
-  { title: "Cinema Paradiso", year: 1988 },
-  { title: "The Lives of Others", year: 2006 },
-  { title: "Grave of the Fireflies", year: 1988 },
-  { title: "Paths of Glory", year: 1957 },
-  { title: "Django Unchained", year: 2012 },
-  { title: "The Shining", year: 1980 },
-  { title: "WALL·E", year: 2008 },
-  { title: "American Beauty", year: 1999 },
-  { title: "The Dark Knight Rises", year: 2012 },
-  { title: "Princess Mononoke", year: 1997 },
-  { title: "Aliens", year: 1986 },
-  { title: "Oldboy", year: 2003 },
-  { title: "Once Upon a Time in America", year: 1984 },
-  { title: "Witness for the Prosecution", year: 1957 },
-  { title: "Das Boot", year: 1981 },
-  { title: "Citizen Kane", year: 1941 },
-  { title: "North by Northwest", year: 1959 },
-  { title: "Vertigo", year: 1958 },
-  {
-    title: "Star Wars: Episode VI - Return of the Jedi",
-    year: 1983,
-  },
-  { title: "Reservoir Dogs", year: 1992 },
-  { title: "Braveheart", year: 1995 },
-  { title: "M", year: 1931 },
-  { title: "Requiem for a Dream", year: 2000 },
-  { title: "Amélie", year: 2001 },
-  { title: "A Clockwork Orange", year: 1971 },
-  { title: "Like Stars on Earth", year: 2007 },
-  { title: "Taxi Driver", year: 1976 },
-  { title: "Lawrence of Arabia", year: 1962 },
-  { title: "Double Indemnity", year: 1944 },
-  {
-    title: "Eternal Sunshine of the Spotless Mind",
-    year: 2004,
-  },
-  { title: "Amadeus", year: 1984 },
-  { title: "To Kill a Mockingbird", year: 1962 },
-  { title: "Toy Story 3", year: 2010 },
-  { title: "Logan", year: 2017 },
-  { title: "Full Metal Jacket", year: 1987 },
-  { title: "Dangal", year: 2016 },
-  { title: "The Sting", year: 1973 },
-  { title: "2001: A Space Odyssey", year: 1968 },
-  { title: "Singin' in the Rain", year: 1952 },
-  { title: "Toy Story", year: 1995 },
-  { title: "Bicycle Thieves", year: 1948 },
-  { title: "The Kid", year: 1921 },
-  { title: "Inglourious Basterds", year: 2009 },
-  { title: "Snatch", year: 2000 },
-  { title: "3 Idiots", year: 2009 },
-  { title: "Monty Python and the Holy Grail", year: 1975 },
-];
+
 export default AddEditPost;
