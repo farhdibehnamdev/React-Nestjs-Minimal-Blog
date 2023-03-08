dd45d8628895fe5a5bf561d40ffcbc8adf058136
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-03-08 22:49:32 +0330

    chore:Added handlePostAdd function.

--- admin-panel/src/components/posts/AddPost.tsx
+++ admin-panel/src/components/posts/AddPost.tsx
@@ -1,5 +1,8 @@
-import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import { useState } from "react";
+import { postType } from "src/config/api/postsApi/postsApi";
+import useThunk from "src/hooks/useThunk";
+import { addPostThunk } from "src/store/thunks/postThunks/addPostThunk";
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 import { BreadcrumbsType } from "../common/BreadcrumbsProps";
 import AddEditPost from "./AddEditPost";
@@ -7,13 +10,20 @@ const breadcrumbTitles: BreadcrumbsType = {
   titles: ["پست", "افزودن پست"],
 };
 const AddPost = function () {
+  const [createPost, isCreatingPost, isCreatedPostError] =
+    useThunk(addPostThunk);
+  const [open, setOpen] = useState<boolean>(false);
+  const handlePostAdd = function (formData: postType) {
+    createPost(formData);
+    setOpen(true);
+  };
   return (
     <>
       <Grid item mb={5.2}>
         <h1>افزودن پست</h1>
         <Breadcrumbs {...breadcrumbTitles} />
       </Grid>
-      <AddEditPost />
+      <AddEditPost typeOperation="Add" onAdd={handlePostAdd} />
     </>
   );
 };
