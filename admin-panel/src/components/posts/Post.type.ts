07a36fd5e1e843b76a8f8688f754d805b1a5e5cd
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-03-09 23:48:25 +0330

    chore:Implemented post type for submiting.

---
+++ admin-panel/src/components/posts/Post.type.ts
@@ -0,0 +1,10 @@
+export type formPostValidationType = {
+  title: string;
+  body: string;
+  isPublished: boolean;
+  publishedAt: string;
+  mainImageUrl: string;
+  categoryId: number;
+  userId: string;
+  tags: string[];
+};
