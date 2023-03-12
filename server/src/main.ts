54af75cbd0f716a3e835093be1dd478727862a1f
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-03-12 23:09:24 +0330

    chore:Added enableCors in main file.

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,15 +4,20 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const configService = app.get(ConfigService);
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
-      forbidNonWhitelisted: true,
+      // forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
   await app.listen(configService.get<string>('APP_PORT'));
