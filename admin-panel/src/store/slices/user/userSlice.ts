dd48ed840b7eefc116135294bd29fcc9f4b7f87e
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-03-16 23:37:40 +0330

    chore:Implemented userSlice.

---
+++ admin-panel/src/store/slices/user/userSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { usersDataType } from "src/config/api/usersApi/usersApi";
+import { fetchUsers } from "src/store/thunks/userThunks/fetchUsers";
+type userState = {
+  isLoading: false;
+  data: usersDataType[];
+  count: number;
+  error: string | null;
+};
+const initialState: userState = {
+  isLoading: false,
+  data: [],
+  count: 0,
+  error: null,
+};
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    //============ Fetch Tags =================
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      state.isLoading = false;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload.data;
+      state.count = action.payload.count;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message as string;
+    });
+  },
+});
+export const userReducer = userSlice.reducer;
