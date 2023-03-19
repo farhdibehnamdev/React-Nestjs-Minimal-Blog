f41dafa044c2657db0e057e951b030cd0dedabd5
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-03-19 23:58:54 +0330

    refactor:Removed extra else from code.

--- admin-panel/src/store/thunks/messageThunks/fetchSentMessages.ts
+++ admin-panel/src/store/thunks/messageThunks/fetchSentMessages.ts
@@ -1,19 +1,27 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { fetchSentMessagesData } from "src/config/api/messagesApi/messagesApi";
+import { createAsyncThunk, AsyncThunk } from "@reduxjs/toolkit";
+import { fetchMessagesData } from "./api";
+import { fetchMessagesArgs, messagesDataType } from "./messages.type";
 
-const fetchSentMessages = createAsyncThunk(
-  "message/fetch",
-  async (arg: any) => {
-    let response: undefined | any[any];
-    if (arg) {
-      response = await fetchSentMessagesData(arg.all, arg.userId, {
-        pagination: arg.pagination,
-        title: arg.title,
-      });
-    } else {
-      response = await fetchSentMessagesData(arg.all, arg.userId);
+export const fetchSentMessages: AsyncThunk<
+  messagesDataType,
+  fetchMessagesArgs,
+  {}
+> = createAsyncThunk(
+  "message/fetchSentMessages",
+  async (args, { rejectWithValue }) => {
+    try {
+      const response = await fetchMessagesData(
+        args.all,
+        args.userId,
+        args.messageType,
+        {
+          pagination: args.pagination,
+          title: args.title,
+        }
+      );
+      return response.data;
+    } catch (err: fetchMessagesData) {
+      return rejectWithValue(err.response.data);
     }
-    return response.data;
   }
 );
-export { fetchSentMessages };
