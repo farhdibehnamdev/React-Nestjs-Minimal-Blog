51729251cd6a89372d00621dd0f38028e5c62fc6
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-03-19 23:59:49 +0330

    chore:Defined types for sent and received messages.

---
+++ admin-panel/src/store/thunks/messageThunks/messages.type.ts
@@ -0,0 +1,8 @@
+export type messagesDataType = any[];
+export type fetchMessagesArgs = {
+  all?: boolean;
+  userId?: string;
+  messageType?: string;
+  pagination?: number;
+  title?: string;
+};
