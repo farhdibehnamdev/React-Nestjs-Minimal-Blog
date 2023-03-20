d6d307562c272efce38996f067bdc2a0e2a05f34
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-03-20 23:45:10 +0330

    refactor: simplify addTag export
    - Remove separate export statement for addTag
    - Use named export for addTag directly in its declaration

--- admin-panel/src/store/thunks/tagThunks/addTag.ts
+++ admin-panel/src/store/thunks/tagThunks/addTag.ts
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { postTagData, postTagsType } from "src/config/api/tagsApi/tagsApi";
-const addTag = createAsyncThunk("tag/add", async (tag: postTagsType) => {
+export const addTag = createAsyncThunk("tag/add", async (tag: postTagsType) => {
   const response = await postTagData(tag);
   return response.data;
 });
-
-export { addTag };
