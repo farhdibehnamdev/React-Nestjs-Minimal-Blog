6cfcd222640a2324ec3648527b466004661bb065
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-03-24 23:26:30 +0330

    feat:Implemented createMessage thunk function.

--- admin-panel/src/store/thunks/messageThunks/createMessage.ts
+++ admin-panel/src/store/thunks/messageThunks/createMessage.ts
@@ -3,16 +3,21 @@ import { messagesDataType } from "src/components/common/common.type";
 import { createMessageApi } from "src/config/api/messagesApi/messagesApi";
 import { fetchMessagesArgs } from "./messages.type";
 
-export const createMessageThunk = createAsyncThunk<
-  { data: messagesDataType },
-  fetchMessagesArgs,
-  { rejectValue: { errorMessage: string } }
->("message/create", async ({ args, thunkApi }: any) => {
-  try {
-    const { userId, messageTitle, messageBody, receive } = args;
-    const response = await createMessageApi(messageTitle, messageBody);
-    return response.data;
-  } catch (err: any) {
-    return thunkApi.rejectWithValue(err.response.data);
+export const createMessageThunk = createAsyncThunk(
+  "message/create",
+  async ({ args, thunkApi }: any) => {
+    try {
+      const { senderId, messageTitle, messageBody, users: receivers } = args;
+      console.log("args ::", args);
+      const response = await createMessageApi(
+        senderId,
+        messageTitle,
+        messageBody,
+        receivers
+      );
+      return response.data;
+    } catch (err: any) {
+      return thunkApi.rejectWithValue(err.response.data);
+    }
   }
-});
+);
