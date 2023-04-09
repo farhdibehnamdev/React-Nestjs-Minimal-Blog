35f771dc387aa9679ad275d61489df7fecf3d4d6
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-04-09 23:35:01 +0330

    feat:Added userUrl and messageUrl

--- admin-panel/src/config/constants/index.ts
+++ admin-panel/src/config/constants/index.ts
@@ -2,9 +2,16 @@ export const URLS = {
   tagUrl: "api/tag/",
   categoryUrl: "api/category/",
   postUrl: "api/article/",
+  userUrl: "api/auth/users",
+  messageUrl: "api/message",
 };
 
 export type paginationOptionType = {
   offset: number;
   limit: number;
 };
+
+// export enum messageType {
+//   sentMessage,
+//   receivedMessage,
+// }
