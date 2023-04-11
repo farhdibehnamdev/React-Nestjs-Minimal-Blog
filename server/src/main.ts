5eed20eaf21856c4bbe6f411a11c2b815fb8bce8
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-04-11 22:15:47 +0330

    fix:Fixed cors error by adding content-type.

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    allowedHeaders: 'GET,HEAD,PUT,PATCH,POST,DELETE,Content-Type',
+    credentials: true,
+  });
+
   const configService = app.get(ConfigService);
   app.enableVersioning({
     type: VersioningType.URI,
