f890c5899042a46ed977c1d9c2ca084077c3fed3
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-04-13 23:19:31 +0330

    feat: Added success or failure message for submitting form.

--- admin-panel/src/components/posts/AddPost.tsx
+++ admin-panel/src/components/posts/AddPost.tsx
@@ -6,6 +6,8 @@ import { addPostThunk } from "src/store/thunks/postThunks/addPostThunk";
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 import { BreadcrumbsType } from "../common/BreadcrumbsProps";
 import AddEditPost from "./AddEditPost";
+import Alert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar/Snackbar";
 const breadcrumbTitles: BreadcrumbsType = {
   titles: ["پست", "افزودن پست"],
 };
@@ -17,12 +19,38 @@ const AddPost = function () {
     createPost(formData);
     setOpen(true);
   };
+  const handleClose = (event: React.SyntheticEvent | Event, reason: string) => {
+    if (reason === "clickAway") return;
+    setOpen(false);
+  };
   return (
     <>
+      {isCreatedPostError ? (
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          open={open}
+          autoHideDuration={3000}
+          onClose={handleClose}
+        >
+          <Alert severity="error"> افزودن پست با خطا مواجه شد </Alert>
+        </Snackbar>
+      ) : (
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          open={open}
+          autoHideDuration={6000}
+          onClose={handleClose}
+        >
+          <Alert severity="success" variant="filled" sx={{ width: "100%" }}>
+            پست با موفقیت ایجاد شد
+          </Alert>
+        </Snackbar>
+      )}
       <Grid item mb={5.2}>
         <h1>افزودن پست</h1>
         <Breadcrumbs {...breadcrumbTitles} />
       </Grid>
+
       <AddEditPost typeOperation="Add" onAdd={handlePostAdd} />
     </>
   );
