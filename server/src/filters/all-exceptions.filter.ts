c347b0d33b05a3bb6fedb5e627f5ea86a623dc9d
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-04-15 23:44:36 +0330

    feat:Added exception handling logic.

---
+++ server/src/filters/all-exceptions.filter.ts
@@ -0,0 +1,29 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+    const statusCode = Number(status);
+    console.log(response);
+    response.status(status).json({
+      statusCode: statusCode,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
