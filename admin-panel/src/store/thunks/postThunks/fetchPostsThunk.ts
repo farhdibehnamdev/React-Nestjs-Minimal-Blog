08089c9c2ca7d360a7c049164bd295237da3047f
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-04-15 23:45:42 +0330

    feat:Added types for handling fetch Post request.

--- admin-panel/src/store/thunks/postThunks/fetchPostsThunk.ts
+++ admin-panel/src/store/thunks/postThunks/fetchPostsThunk.ts
@@ -1,16 +1,35 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { fetchPosts } from "src/config/api/postsApi/postsApi";
 
-export const fetchPostsThunk = createAsyncThunk(
+type paginationType = {
+  limit: number;
+  offset: number;
+};
+
+type paginationTitleType = {
+  pagination: paginationType;
+  title: string;
+};
+
+const postType: paginationTitleType = {
+  pagination: { limit: 5, offset: 0 },
+  title: "",
+};
+const fetchPostsThunk = createAsyncThunk(
   "post/fetchPosts",
   async (arg: any) => {
     let response: undefined | any[any];
     if (arg) {
-      response = await fetchPosts(arg.pagination, arg.title);
+      postType.title = arg.title;
+      postType.pagination.limit = arg.limit;
+      postType.pagination.offset = arg.offset;
+      response = await fetchPosts(arg.all, postType);
     } else {
-      response = await fetchPosts();
+      response = await fetchPosts(arg.all);
     }
-    console.log("response :::", response.data);
+
     return response.data;
   }
 );
+
+export { fetchPostsThunk };
