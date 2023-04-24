88f73bac25112bb2739b1cf5bc9aac8c2054c4ec
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-04-24 23:36:59 +0330

    refactor: Updated fetchCategories function
    - Rename FetchCategoriesType to categoryTitleAndPagination
    - Add categoryType object with default values for title and pagination
    - Update fetchCategories function to use categoryType object

--- admin-panel/src/store/thunks/categoryThunks/fetchCategories.ts
+++ admin-panel/src/store/thunks/categoryThunks/fetchCategories.ts
@@ -2,19 +2,27 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { fetchCategoriesData } from "src/config/api/categoriesApi/categoriesApi";
 import { paginationOptionType } from "src/config/constants";
 
-type FetchCategoriesType = {
+type categoryTitleAndPagination = {
   pagination: paginationOptionType;
   title: string;
 };
+
+const categoryType: categoryTitleAndPagination = {
+  title: "",
+  pagination: { limit: 5, offset: 0 },
+};
+
 const fetchCategories = createAsyncThunk(
   "category/fetchCategories",
   async (arg: any) => {
     let response: undefined | any[any];
-
     if (arg) {
-      response = await fetchCategoriesData(arg.pagination, arg.title);
+      categoryType.title = arg.title;
+      categoryType.pagination.limit = arg.limit;
+      categoryType.pagination.offset = arg.offset;
+      response = await fetchCategoriesData(arg.all, categoryType);
     } else {
-      response = await fetchCategoriesData(undefined);
+      response = await fetchCategoriesData(arg.all);
     }
 
     return response.data;
