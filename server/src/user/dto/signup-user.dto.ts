befc5f426ada11349718467be2fbdc141a665d9f
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-03 23:20:16 +0330

    refactor:Removed firstName and lastName properties from singupUserDto.

--- server/src/user/dto/signup-user.dto.ts
+++ server/src/user/dto/signup-user.dto.ts
@@ -1,24 +1,18 @@
 import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class SignUpUserDto {
-  @IsNotEmpty()
-  @IsString()
-  firstName: string;
-
-  @IsNotEmpty()
-  @IsString()
-  lastName: string;
-
   @IsString()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
   @IsString()
+  @IsNotEmpty()
   @Length(8)
   password: string;
 
   @IsString()
+  @IsNotEmpty()
   @Length(8)
-  passwordConfirm: string;
+  confirmPassword: string;
 }
