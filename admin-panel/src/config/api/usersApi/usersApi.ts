e62d76654fafb734fd627cc7a59041da8380e84d
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-09 23:07:59 +0330

    feat:Implement signup and signIn user's api by axios.

--- admin-panel/src/config/api/usersApi/usersApi.ts
+++ admin-panel/src/config/api/usersApi/usersApi.ts
@@ -1,31 +1,43 @@
-import { paginationOptionType, URLS } from "src/config/constants";
+import { URLS } from "src/config/constants";
 import api from "../api";
+import {
+  signinUserTypeDto,
+  signupUserTypeDto,
+  usersDataType,
+  userTitleAndPagination,
+} from "src/types/userTypes";
 
 export enum UserRole {
   USER = "user",
   ADMIN = "admin",
 }
-export type usersDataType = {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  role: UserRole;
-};
 
-type userTitleAndPagination = {
-  pagination: paginationOptionType;
-  title: string;
-};
-
-export const fetchUsersData = async (
+export const fetchUsersData = async function (
   all: boolean,
   paginationTitle?: userTitleAndPagination
-) => {
+) {
   const params = { all };
   if (paginationTitle) {
     const { pagination, title } = paginationTitle;
     Object.assign(params, { pagination, title });
   }
   return await api.get<usersDataType>(URLS.userUrl, { params });
+};
+
+export const signupUserApi = async function (signUpUserDto: signupUserTypeDto) {
+  if (signUpUserDto) {
+    return await api.post<signupUserTypeDto>(URLS.signUpUrl, signUpUserDto);
+  }
+};
+
+export const signinUserApi = async function (signinUserDto: signinUserTypeDto) {
+  if (signinUserDto) {
+    return await api.post<string>(URLS.signinUrl, signinUserDto);
+  }
+};
+
+export const verifiedEmailApi = async function (token: string) {
+  if (token) {
+    return await api.get<string>(URLS.verifiedEmailUrl, { params: { token } });
+  }
 };
