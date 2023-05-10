dfa921215e2bee7b62f3a43e67df5e210e1b0cf4
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-10 23:44:16 +0330

    feat:Add a request interceptor to authorization purpose.

--- admin-panel/src/config/api/api.ts
+++ admin-panel/src/config/api/api.ts
@@ -1,11 +1,30 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
-
 const axiosParams = {
   baseURL: "http://localhost:3001/v1/",
-  // process.env.NODE_ENV === "development" ? "http://localhost:3001/v1/" : "/",
+  headers: {
+    "Content-Type": "application/json",
+  },
 };
 
+// process.env.NODE_ENV === "development" ? "http://localhost:3001/v1/" : "/",
+
 const axiosInstance = axios.create(axiosParams);
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accToken");
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+// const withLogger = async <T>(Promise: AxiosPromise<T>) => promise.catch((error:ApiError));
 
 const api = (axios: AxiosInstance) => {
   return {
@@ -21,4 +40,5 @@ const api = (axios: AxiosInstance) => {
       axios.put<T>(url, body, config),
   };
 };
+
 export default api(axiosInstance);
