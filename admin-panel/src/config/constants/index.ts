f74c7261190dc5065de6fca8e93c8b241db47e37
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-10 23:43:02 +0330

    feat:Add verifyTokenUrl , signinUrl,signUpUrl.

--- admin-panel/src/config/constants/index.ts
+++ admin-panel/src/config/constants/index.ts
@@ -2,8 +2,12 @@ export const URLS = {
   tagUrl: "api/tag/",
   categoryUrl: "api/category/",
   postUrl: "api/article/",
-  userUrl: "api/auth/users",
+  userUrl: "auth/users",
   messageUrl: "api/message",
+  signUpUrl: "auth/signup",
+  signinUrl: "auth/signin",
+  verifiedEmailUrl: "auth/verify-email",
+  verifyTokenUrl: "auth/verify-token",
 };
 
 export type paginationOptionType = {
