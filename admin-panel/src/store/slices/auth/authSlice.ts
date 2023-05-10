a2a50d53d2a3b69637746daad9d6237e5cc67ddc
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-10 23:41:39 +0330

    feat:Implement authSlice.

---
+++ admin-panel/src/store/slices/auth/authSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type user = {
+  id: number;
+  email: string;
+  role: string;
+};
+
+export type authState = {
+  isAuthenticated: boolean;
+  userInfo: user | null;
+};
+
+const initialState: authState = {
+  isAuthenticated: false,
+  userInfo: null,
+};
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    signIn: (state, action) => {
+      state.isAuthenticated = true;
+      state.userInfo = action.payload;
+    },
+    signOut: (state, action) => {
+      state.isAuthenticated = false;
+      state.userInfo = null;
+    },
+  },
+});
+export const { signIn, signOut } = authSlice.actions;
+export const authReducer = authSlice.reducer;
