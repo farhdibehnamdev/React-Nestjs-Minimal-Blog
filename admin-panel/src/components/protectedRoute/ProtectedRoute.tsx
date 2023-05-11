71b5761efe1cceebe7d260c1e68385f44f2dafde
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-11 23:45:03 +0330

    refactor:Refactor ProtectedRoute two kind of Public and Protected routes.

---
+++ admin-panel/src/components/protectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,48 @@
+import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useAppSelector } from "src/store/hooks";
+import { authState } from "src/store/slices/auth/authSlice";
+
+type user = {
+  id: number;
+  email: string;
+  role: string;
+};
+export type verifyTokenType = {
+  isAuthenticated: boolean;
+  userInfo: user;
+};
+
+const ProtectedRoute = ({ roles }: any) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { isAuthenticated, userInfo } = useAppSelector(
+    (state: { auth: authState }) => state.auth
+  );
+
+  if (!isAuthenticated || !roles.includes(userInfo?.role)) {
+    navigate("/auth/sign-in", { replace: true, state: { from: location } });
+  }
+
+  return isAuthenticated && roles.includes(userInfo?.role) ? (
+    <Outlet />
+  ) : userInfo?.role ? (
+    <Navigate to="/unauthorized" state={{ from: location }} replace />
+  ) : (
+    <Navigate to="/auth/sign-in" state={{ from: location }} replace />
+  );
+};
+
+export const PublicRoute = () => {
+  const location = useLocation();
+  const { isAuthenticated } = useAppSelector(
+    (state: { auth: authState }) => state.auth
+  );
+
+  return !isAuthenticated ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/" state={{ from: location }} replace />
+  );
+};
+
+export default ProtectedRoute;
