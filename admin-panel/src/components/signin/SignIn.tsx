a582791c4befa9b26f852fd4b613f57e01745951
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-11 23:47:51 +0330

    feat:Implement signIn component.

--- admin-panel/src/components/signin/SignIn.tsx
+++ admin-panel/src/components/signin/SignIn.tsx
@@ -7,9 +7,49 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { signinUserApi } from "src/config/api/usersApi/usersApi";
+import { authState, signIn } from "src/store/slices/auth/authSlice";
+
+type user = {
+  id: number;
+  email: string;
+  role: string;
+};
+
+export interface apiResponse {
+  accessToken: string;
+  refreshToken: string;
+  userInfo: user;
+}
+
+const isApiResponse = function (data: any): data is apiResponse {
+  return "accessToken" in data && "refreshToken" in data;
+};
 
 const SignIn = function () {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleSubmit = async function (
+    event: React.FormEvent<HTMLFormElement>
+  ) {
+    event.preventDefault();
+    const email = event.currentTarget.email.value;
+    const password = event.currentTarget.password.value;
+    const response = await signinUserApi({ email, password });
+    if (isApiResponse(response?.data)) {
+      const { accessToken, refreshToken, userInfo } =
+        response?.data as apiResponse;
+      if (response?.status === 200) {
+        localStorage.setItem("accToken", accessToken);
+        localStorage.setItem("refToken", refreshToken);
+        dispatch(signIn(userInfo));
+        navigate("/");
+      }
+    }
+  };
+
   return (
     <Grid
       sx={{
@@ -26,6 +66,7 @@ const SignIn = function () {
           alignItems: "center",
           marginBottom: "30px",
         }}
+        onSubmit={handleSubmit}
       >
         <Grid item xl={12} mb={6}>
           <img src="../assets/images/logo.png" alt="logo_image" />
