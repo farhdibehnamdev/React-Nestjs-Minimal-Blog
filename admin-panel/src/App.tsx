5ac4dadcfad481a55fb127099e8b85267ceb95cf
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-12 21:39:47 +0330

    refactor:Separate routes to antoher file.

--- admin-panel/src/App.tsx
+++ admin-panel/src/App.tsx
@@ -9,41 +9,18 @@ import {
   ThemeProvider,
   useMediaQuery,
 } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { toggleSidebar } from "./store/slices/toggle/toggleSlice";
 import theme from "./globalStyles/theme";
-import DashboardLayout from "./components/dashboardLayout/DashboardLayout";
-import { Route, Routes } from "react-router-dom";
-import Dashboard from "./components/dashboard/Dashboard";
-import Settings from "./components/settings/Settings";
-import Profile from "./components/profile/Profile";
-import Posts from "./components/posts/Posts";
-import Tags from "./components/tags/Tags";
-import SendMessage from "./components/sendMessage/SendMessage";
-import UserManagement from "./components/userManagement/UserManagement";
-import SignOut from "./components/signout/SignOut";
-import AuthLayout from "./components/authLayout/AuthLayout";
-import SignIn from "./components/signin/SignIn";
-import SignUp from "./components/signup/SignUp";
-import Home from "./components/home/Home";
-import Categories from "./components/categories/Categories";
-import AddPost from "./components/posts/AddPost";
-import PostsLayout from "./components/posts/PostsLayout";
-import EditPost from "./components/posts/EditPost";
-import AddCategory from "./components/categories/AddCategory";
-import EditCategory from "./components/categories/EditCategory";
-import CategoriesLayout from "./components/categories/CategoriesLayout";
-import TagsLayout from "./components/tags/TagsLayout";
-import AddTag from "./components/tags/AddTag";
-import EditTag from "./components/tags/EditTag";
-import ResetPassword from "./components/resetPassword/ResetPassword";
+import { RouteApp } from "./routes/Routes";
+import { useAppSelector } from "./store/hooks";
 const cacheRTL = createCache({
   key: "muirtl",
   stylisPlugins: [prefixer, rtlPlugin],
 });
 
 function App() {
-  const { toggle } = useSelector((state: any) => state.toggle);
+  const { toggle } = useAppSelector((state: any) => state.toggle);
   const dispatch = useDispatch();
   const mediaQ = useMediaQuery(theme.breakpoints.down("lg"));
   const handleClose = () => {
@@ -55,39 +32,7 @@ function App() {
       <CacheProvider value={cacheRTL}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Routes>
-            <Route element={<DashboardLayout />}>
-              <Route element={<Dashboard />}>
-                <Route index element={<Home />} />
-                <Route path="settings" element={<Settings />} />
-                <Route path="profile" element={<Profile />} />
-                <Route path="posts" element={<PostsLayout />}>
-                  <Route index element={<Posts />} />
-                  <Route path="add" element={<AddPost />} />
-                  <Route path="edit/:id" element={<EditPost />} />
-                </Route>
-                <Route path="categories" element={<CategoriesLayout />}>
-                  <Route index element={<Categories />} />
-                  <Route path="add" element={<AddCategory />} />
-                  <Route path="edit/:id" element={<EditCategory />} />
-                </Route>
-                <Route path="tags" element={<TagsLayout />}>
-                  <Route index element={<Tags />} />
-                  <Route path="add" element={<AddTag />} />
-                  <Route path="edit/:id" element={<EditTag />} />
-                </Route>
-                <Route path="send-message" element={<SendMessage />} />
-                <Route path="user-management" element={<UserManagement />} />
-
-                <Route path="sign-out" element={<SignOut />} />
-              </Route>
-            </Route>
-            <Route path="auth" element={<AuthLayout />}>
-              <Route path="sign-in" element={<SignIn />} />
-              <Route path="sign-up" element={<SignUp />} />
-              <Route path="reset-password" element={<ResetPassword />} />
-            </Route>
-          </Routes>
+          <RouteApp />
           {mediaQ && (
             <Backdrop
               sx={{
