09bad149bfc19603e4f6d6ec4843e8df3c18d49f
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-16 17:26:35 +0330

    ✨ feat:Add Alert material ui to Notice component.

---
+++ admin-panel/src/components/notice/Notice.tsx
@@ -0,0 +1,15 @@
+import { Snackbar, Alert } from "@mui/material";
+export const Notice = function ({ open, alertType, message, onClose }: any) {
+  return (
+    <Snackbar
+      open={open}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      autoHideDuration={3000}
+      onClose={onClose}
+    >
+      <Alert onClose={onClose} severity={alertType} sx={{ width: "100%" }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
