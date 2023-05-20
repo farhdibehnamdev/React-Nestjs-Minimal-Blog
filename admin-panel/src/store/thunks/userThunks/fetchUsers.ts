3c96454f61f925e388e6e473a774656123bdaa60
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-20 22:42:12 +0330

    ♻️ refactor:Refactor fetchUsers function to improve argument handling and utilize a dedicated type.

--- admin-panel/src/store/thunks/userThunks/fetchUsers.ts
+++ admin-panel/src/store/thunks/userThunks/fetchUsers.ts
@@ -1,13 +1,23 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { fetchUsersData } from "src/config/api/usersApi/usersApi";
+import { paginationOptionType } from "src/config/constants";
+
+type userTitleAndPagination = {
+  pagination: paginationOptionType;
+  title: string;
+};
+const usersType: userTitleAndPagination = {
+  title: "",
+  pagination: { limit: 5, offset: 0 },
+};
 
 const fetchUsers = createAsyncThunk("users/fetch", async (arg: any) => {
   let response: undefined | any[any];
-  if (arg) {
-    response = await fetchUsersData(arg.all, {
-      pagination: arg.pagination,
-      title: arg.title,
-    });
+  if (!arg.all) {
+    usersType.title = arg.title;
+    usersType.pagination.limit = arg.limit;
+    usersType.pagination.offset = arg.offset;
+    response = await fetchUsersData(arg.all, usersType);
   } else {
     response = await fetchUsersData(arg.all);
   }
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -7,17 +7,35 @@ import {
   Query,
   HttpCode,
   UseGuards,
+  Patch,
+  Put,
+  Param,
 } from '@nestjs/common';
 import { SigninUserDto } from './dto/login.dto';
 import { SignUpUserDto } from './dto/signup-user.dto';
 import { JWTTokens, UserService, createUserStatus } from './user.service';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
 import { PaginationQueryDto } from 'src/common/pagination-query.dto';
+import User, { UserRole } from './entities/user.entity';
 import { usersDataAndCount } from './types/user.type';
-import { UserRole } from './entities/user.entity';
 import { Role } from './decorators/role';
 import { AccessTokenGuard } from './guard/access-token.guard';
 import { RoleGuard } from './guard/authorization.guard';
+import { FindOptionsWhere, Like } from 'typeorm';
+import {
+  PatchUserManagementDto,
+  PutUserManagementDto,
+} from './dto/userManagement.dto';
+
+type paginationTitle = {
+  pagination: PaginationQueryDto;
+  title: string;
+};
+
+const paginationTitleType: paginationTitle = {
+  pagination: { limit: 5, offset: 0 },
+  title: '',
+};
 
 @Controller('auth/')
 export class UserController {
@@ -49,14 +67,34 @@ export class UserController {
   @Get('users')
   async findAll(
     @Query('all') all: boolean,
-    @Query() pagination: PaginationQueryDto,
-    @Query('title') title?: string,
+    @Query() paginationTitleType: paginationTitle,
   ): Promise<usersDataAndCount> {
     if (all) {
-      const ress = await this.userService.findAll();
-      return ress;
+      const usersData = await this.userService.findAll(
+        all,
+        paginationTitleType.pagination,
+        {
+          firstName: paginationTitleType.title,
+        },
+      );
+      return usersData;
+    } else if (
+      paginationTitleType.title === null ||
+      paginationTitleType.title === undefined ||
+      paginationTitleType.title === ''
+    ) {
+      return this.userService.paginate(paginationTitleType.pagination);
+    } else {
+      const searchCriteria: FindOptionsWhere<User> = {
+        firstName: Like(`%${paginationTitleType.title}%`),
+      };
+
+      return this.userService.findAll(
+        all,
+        paginationTitleType.pagination,
+        searchCriteria,
+      );
     }
-    return { data: null, count: 0 };
   }
 
   @Version('1')
@@ -77,5 +115,26 @@ export class UserController {
       tokens.accessToken,
       tokens.refreshToken,
     );
+  }
+
+  @Role(UserRole.USER)
+  @Role(UserRole.ADMIN)
+  @Version('1')
+  @Put('user/update')
+  async putUpdate(@Body() putUserManagementDto: PutUserManagementDto) {
+    return await this.userService.putUpdate(putUserManagementDto);
+  }
+
+  @Role(UserRole.USER)
+  @Role(UserRole.ADMIN)
+  @Version('1')
+  @Patch('user/update/:id')
+  async patchUpdate(
+    @Param('id') id: string,
+    @Body() patchUserManagementDto: PatchUserManagementDto,
+  ) {
+    console.log('patch update :: ', id);
+    console.log('patchUserManagementDto :: ', patchUserManagementDto);
+    return await this.userService.patchUpdate(id, patchUserManagementDto);
   }
 }
