baf15aa837ebe0d9b83b2c9681ca7694fdefe467
farhdibehnamdev <behnammfarhadi@gmail.com>
2023-05-22 18:48:44 +0330

    fix:Fix role decorator by array.

--- server/src/user/decorators/role.ts
+++ server/src/user/decorators/role.ts
@@ -2,4 +2,4 @@ import { SetMetadata } from '@nestjs/common';
 import { UserRole } from '../entities/user.entity';
 
 export const ROLE_KEY = 'role';
-export const Role = (role: UserRole) => SetMetadata(ROLE_KEY, role);
+export const Role = (...role: UserRole[]) => SetMetadata(ROLE_KEY, role);
