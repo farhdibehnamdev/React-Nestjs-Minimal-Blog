1aec9de77363dd395a82a908a378fa14d7ad9036
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-05-26 23:09:21 +0330

    ✨ feat:Implement userProfileThunk(#1).

---
+++ admin-panel/src/store/thunks/userThunks/userProfileThunk.ts
@@ -0,0 +1,24 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { userProfileApi } from "src/config/api/usersApi/usersApi";
+import { usersDataType } from "src/types/userTypes";
+
+export type UserProfileBodyData = {
+  firstName: string;
+  lastName: string;
+  newPassword: string;
+  avatar: FileList;
+};
+
+type UserProfileType = {
+  id: string;
+  data: UserProfileBodyData;
+};
+
+export const userProfileThunk = createAsyncThunk(
+  "user/profileEdit",
+  async (arg: UserProfileType): Promise<usersDataType> => {
+    const { id, data } = arg;
+    const response = await userProfileApi(id, data);
+    return response.data;
+  }
+);
