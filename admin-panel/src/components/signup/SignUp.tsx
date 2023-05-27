9a3d697afda9425f23d40a8600b61390d064601c
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-05-27 21:11:53 +0330

    ✨ feat:Implement signup form.

--- admin-panel/src/components/signup/SignUp.tsx
+++ admin-panel/src/components/signup/SignUp.tsx
@@ -1,18 +1,77 @@
-import { Button, Divider, Grid, TextField, Typography } from "@mui/material";
+import {
+  Alert,
+  Button,
+  Divider,
+  Grid,
+  Snackbar,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useForm } from "react-hook-form";
 import { signupUserApi } from "src/config/api/usersApi/usersApi";
 import { useNavigate } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { LoadingButton } from "@mui/lab";
+import { useState } from "react";
+import SendIcon from "@mui/icons-material/Send";
+import { AlertColor } from "@mui/lab/Alert";
+import { Notice } from "../notice/Notice";
+import { useAlert } from "src/hooks/useAlert";
+type SignupFormData = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const schema = yup.object({
+  email: yup.string().email().required("فیلد ایمیل اجباری است"),
+  password: yup.string().required("فیلد پسورد اجباری است"),
+  confirmPassword: yup.string().required("فیلد تکرار پسورد اجباری است"),
+});
+
 const SignUp = function () {
+  const { showNotice, message, open, noticeType } = useAlert();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignupFormData>({ resolver: yupResolver(schema) });
   const navigate = useNavigate();
-  const handleSubmit = async function (
-    event: React.FormEvent<HTMLFormElement>
-  ) {
-    const email = event.currentTarget.email.value;
-    const password = event.currentTarget.password.value;
-    const confirmPassword = event.currentTarget.confirmPassword.value;
-    event.preventDefault();
-    const response = await signupUserApi({ email, password, confirmPassword });
-    if (response?.status === 201) {
-      navigate("/auth/verify-email");
+  const onSubmit = async function (data: SignupFormData) {
+    const email = data.email;
+    const password = data.password;
+    const confirmPassword = data.confirmPassword;
+
+    try {
+      setIsLoading(true);
+      const response = await signupUserApi({
+        email,
+        password,
+        confirmPassword,
+      });
+      if (response?.status === 200) {
+        showNotice("", "success");
+        setIsLoading(false);
+        navigate("/auth/verify-email");
+      }
+    } catch (error: any) {
+      if (error?.response.status === 400) {
+        setIsLoading(false);
+        showNotice("کاربر با ایمیل مورد نظر قبلآ ثبت شده است", "error");
+      } else {
+        setIsLoading(false);
+        showNotice("خطایی در ارسال اطلاعات بوجود آمد", "error");
+      }
     }
   };
 
@@ -25,8 +84,9 @@ const SignUp = function () {
         width: "500px",
       }}
     >
+      <Notice />
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit((data) => onSubmit(data))}
         style={{
           display: "flex",
           flexDirection: "column",
@@ -47,39 +107,51 @@ const SignUp = function () {
         </Grid>
         <Grid container item mb={3}>
           <TextField
+            {...register("email", { required: true })}
             placeholder="ایمیل خود را وارد کنید"
             name="email"
             type="email"
             fullWidth
+            error={!!errors.email}
+            helperText={errors.email?.message}
           />
         </Grid>
         <Grid container item mb={3}>
           <TextField
+            {...register("password", { required: true })}
             placeholder="رمز عبور خود را وارد کنید"
             name="password"
             type="password"
             fullWidth
+            error={!!errors.password}
+            helperText={errors.password?.message}
           />
         </Grid>
         <Grid container item mb={3}>
           <TextField
+            {...register("confirmPassword", { required: true })}
             placeholder="تکرار رمز عبور"
             name="confirmPassword"
             type="password"
             fullWidth
+            error={!!errors.confirmPassword}
+            helperText={errors.confirmPassword?.message}
           />
         </Grid>
 
         <Grid container justifyContent="center">
-          <Button
+          <LoadingButton
+            size="small"
+            endIcon={<SendIcon />}
+            loading={isLoading}
+            loadingPosition="end"
+            variant="contained"
             type="submit"
             fullWidth
-            size="large"
-            variant="contained"
-            color="primary"
+            sx={{ padding: "10px", fontSize: "15px" }}
           >
             ثبت نام
-          </Button>
+          </LoadingButton>
         </Grid>
       </form>
       <Divider />
